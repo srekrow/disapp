3e04b5f0f28b71cff4fbfee8d48e530fd42b502d
srekrow <54987210+srekrow@users.noreply.github.com>
2025-09-21 14:07:23 +0800

    Update DeviceAdminReceiver.java

--- app/src/main/java/com/miui/hybrid/DeviceAdminReceiver.java
+++ app/src/main/java/com/miui/hybrid/DeviceAdminReceiver.java
@@ -1,10 +1,13 @@
 package com.miui.hybrid;
 
-import android.app.admin.DeviceAdminReceiver;
+// 我们不再需要导入 DeviceAdminReceiver 了，所以下一行被删除了
+// import android.app.admin.DeviceAdminReceiver;
+
 import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+// 在这里，我们使用完整的类名来避免命名冲突
 public class DeviceAdminReceiver extends android.app.admin.DeviceAdminReceiver {
 
     @Override
